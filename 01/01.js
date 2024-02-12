4d340c22abb6629e419c9d4f6e214aae91607a83
Prakhar Pratyush <prakhar.pratyush8010@gmail.com>
2024-02-12 12:55:59 +0530

    Project initialization method .Read 01.js file

---
+++ 01/01.js
@@ -0,0 +1,18 @@
+//1st method for a react project
+//react is not a framework it is a library(react and react-dom are for web pages)
+//react and react-native are used for app development
+//npm-node package manager
+//npx-node package executor
+//npx-create-react-app to initiazlise a react project(this is a really slow process there are alternatives for it one such is Vite or Parcel)
+//A directory '01_basicreact' using create-react-app is initialised
+//Inside it the package.json file is the entry point that contains versions and dependencies
+//npm run start to start project in localhost
+//npm run build to build the project
+
+
+
+// method using vite
+//vite is a bundler
+//npm create vite@latest
+//before running a vite project we must first install the node-module folder using npm i or npm install
+//to run - npm run dev

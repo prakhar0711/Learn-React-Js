5a2ead951da5053be515a10bb1312a9705dc7998
Prakhar Pratyush <prakhar.pratyush8010@gmail.com>
2024-02-13 14:40:38 +0530

    bg colour changer

--- 06_bgChanger/src/App.jsx
+++ 06_bgChanger/src/App.jsx
@@ -1,19 +1,21 @@
 import { useState } from "react";
 import "./App.css";
 
+/**
+ * App component to manage color state and display color-changing buttons
+ */
 function App() {
-  //creating useState to manage color
+  // State to manage color
   const [color, setColor] = useState("olive");
 
   return (
     <>
-      {/* Creating the viewport div */}
+      {/* Viewport div */}
       <div className="w-full h-screen" style={{ backgroundColor: color }}>
-        {/* creating and reserving the bottom bar width */}
+        {/* Bottom bar */}
         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-          {/* creting the bar */}
+          {/* Color-changing buttons */}
           <div className="flex flex-wrap justify-center gap-2 shadow-xl bg-white p-3 rounded-2xl">
-            {/* creating the buttons */}
             <button
               onClick={() => setColor("red")}
               className="outline-none px-4 py-1 rounded-full shadow-2xl text-white"
@@ -34,6 +36,13 @@ function App() {
               style={{ backgroundColor: "blue" }}
             >
               Blue
+            </button>
+            <button
+              onClick={() => setColor("#b0b0f5")}
+              className="outline-none px-4 py-1 rounded-full shadow-2xl text-white"
+              style={{ backgroundColor: "#b0b0f5" }}
+            >
+              Lavender
             </button>
           </div>
         </div>

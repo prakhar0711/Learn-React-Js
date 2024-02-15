f41afe35f62aa81d41056d2cabb1382336ea475b
Prakhar Pratyush <prakhar.pratyush8010@gmail.com>
2024-02-15 13:11:51 +0530

    tictactoe game

--- 09_tictactoe/src/App.jsx
+++ 09_tictactoe/src/App.jsx
@@ -4,56 +4,86 @@ function App() {
   let [state, setState] = useState(Array(9).fill(null));
   let [isXTurn, setisXTurn] = useState(true);
 
-  const checkWin = useCallback(() => {
-    const copyState = [...state];
-    if (
-      copyState[0] == copyState[1] &&
-      copyState[1] == copyState[2] &&
-      copyState[2] != null
-    ) {
-      alert(`${copyState[2]} wins`);
-    } else if (
-      copyState[3] == copyState[4] &&
-      copyState[4] == copyState[5] &&
-      copyState[5] != null
-    ) {
-      alert(`${copyState[5]} wins`);
-    } else if (
-      copyState[6] == copyState[7] &&
-      copyState[7] == copyState[8] &&
-      copyState[8] != null
-    ) {
-      alert(`${copyState[8]} wins`);
-    } else if (
-      copyState[0] == copyState[4] &&
-      copyState[4] == copyState[8] &&
-      copyState[8] != null
-    ) {
-      alert(`${copyState[8]} wins`);
-    } else if (
-      copyState[2] == copyState[4] &&
-      copyState[4] == copyState[6] &&
-      copyState[6] != null
-    ) {
-      alert(`${copyState[6]} wins`);
-    }
-  }, [state]);
+  const checkWin = useCallback(
+    (copyState) => {
+      // const copyState = [...state];
+      if (
+        copyState[0] == copyState[1] &&
+        copyState[1] == copyState[2] &&
+        copyState[2] != null
+      ) {
+        alert(`${copyState[2]} wins`);
+      } else if (
+        copyState[3] == copyState[4] &&
+        copyState[4] == copyState[5] &&
+        copyState[5] != null
+      ) {
+        alert(`${copyState[5]} wins`);
+      } else if (
+        copyState[6] == copyState[7] &&
+        copyState[7] == copyState[8] &&
+        copyState[8] != null
+      ) {
+        alert(`${copyState[8]} wins`);
+      } else if (
+        copyState[0] == copyState[4] &&
+        copyState[4] == copyState[8] &&
+        copyState[8] != null
+      ) {
+        alert(`${copyState[8]} wins`);
+      } else if (
+        copyState[2] == copyState[4] &&
+        copyState[4] == copyState[6] &&
+        copyState[6] != null
+      ) {
+        alert(`${copyState[6]} wins`);
+      } else if (
+        copyState[0] == copyState[3] &&
+        copyState[3] == copyState[6] &&
+        copyState[6] != null
+      ) {
+        alert(`${copyState[6]} wins`);
+      } else if (
+        copyState[1] == copyState[4] &&
+        copyState[4] == copyState[7] &&
+        copyState[7] != null
+      ) {
+        alert(`${copyState[7]} wins`);
+      } else if (
+        copyState[2] == copyState[5] &&
+        copyState[5] == copyState[8] &&
+        copyState[8] != null
+      ) {
+        alert(`${copyState[8]} wins`);
+      }
+    },
+    [state]
+  );
   const handleClick = useCallback(
     (index) => {
       const copyState = [...state];
-      copyState[index] = isXTurn ? "X" : "O";
+      if (copyState[index] == null) {
+        copyState[index] = isXTurn ? "X" : "O";
+      } else {
+        alert("box already filled");
+      }
       setState(copyState);
       setisXTurn(!isXTurn);
-      checkWin();
+      setTimeout(() => {
+        checkWin(copyState);    //setTimeout is not a good thing to do here look for other solutions as the alert msg used to get displayed before marking the final X so i used setTimeout on it
+      }, 100);
     },
     [checkWin, isXTurn, state]
   );
-
+  const resetBoard = () => {
+    setState(Array(9).fill(null));
+  };
   // useEffect(() => {
   //   checkWin();
   // }, [state]);
   return (
     <>
+      <div>(X goes First)</div>
       <div className="rows">
         <div
           className="boxes"
@@ -132,6 +162,13 @@ function App() {
           {state[8]}
         </div>
       </div>
+      <button
+        onClick={() => {
+          resetBoard();
+        }}
+      >
+        Reset
+      </button>
     </>
   );
 }

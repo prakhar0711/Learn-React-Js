b2826b094e6e71b9fe217474e957924d4f9bcb04
Prakhar Pratyush <prakhar.pratyush8010@gmail.com>
2024-02-23 12:20:10 +0530

    todo-app

--- 08_todo/src/App.jsx
+++ 08_todo/src/App.jsx
@@ -2,41 +2,52 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todos, settodos] = useState([]);
-  const todolist = (e) => {
-    setTodo(e.target.value);
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+  const handleChange = (event) => {
+    setTask(event.target.value);
+    // console.log(task);
   };
 
-  const displaytodo = () => {
-    settodos([...todos, todo]);
-    setTodo("");
-  };
+  function addTask() {
+    setTaskList((taskList) => {
+      const updatedList = [...taskList, task];
+      setTask("");
+      return updatedList;
+    });
+  }
+  function deleteTask(index) {
+    setTaskList((prevTaskList) => {
+      const updateList = [...prevTaskList];
+      updateList.splice(index, 1);
+      return updateList;
+      
+    });
+  }
   return (
     <>
-      <div>
-        <div>
-          <input
-            onChange={todolist}
-            value={todo}
-            type="text"
-            placeholder="Title"
-          />
-        </div>
-        <div>
-          <button onClick={displaytodo}>Add</button>
-        </div>
-        <div>
-          <p>ToDo for the day</p>
-          <div>
-            {todos.map((todo, index) => (
-              <div key={index}>{todo}</div>
-              
-            ))}
-            
-          </div>
-        </div>
-      </div>
+      <input
+        type="text"
+        value={task}
+        placeholder="enter task"
+        onChange={handleChange}
+      />
+      <button type="submit" onClick={addTask}>
+        Add
+      </button>
+      <ul>
+        {taskList != [] &&
+          taskList.map((data, index) => {
+            return (
+              <>
+                <li key={index}>
+                  {data}{" "}
+                  <button onClick={() => deleteTask(index)}>Delete</button>
+                </li>
+              </>
+            );
+          })}
+      </ul>
     </>
   );
 }
